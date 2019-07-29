fcdfa9a7f8210e476e45e956c556f4a280252248
wediariasantana <40694883+wediariasantana@users.noreply.github.com>
2019-07-29 09:34:50 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$host       = 'localhost';
+$user       = 'root';
+$password   = '';
+$db         = 'db_android';
+
+
+$con = mysqli_connect($host, $user, $password, $db) or die(mysqli_error());
+?>
